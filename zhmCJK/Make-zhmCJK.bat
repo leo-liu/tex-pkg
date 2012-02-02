61bacee7eaabaec82124edc500dbbda372e182ee
leoliu pku <leoliu.pku@gmail.com@77a52a0d-5b5a-0024-7867-7f92eaad0685>
2012-02-02 06:22:49 +0000

    增加 CJKpunct 调用。 代码改用 dtx 格式。文档未编写。

---
+++ zhmCJK/Make-zhmCJK.bat
@@ -0,0 +1,12 @@
+if exist zhmCJK.sty del zhmCJK.sty
+if exist zhmCJK.pdf del zhmCJK.pdf
+tex zhmCJK.ins
+latex zhmCJK.dtx
+makeindex -s gind zhmCJK.idx
+makeindex -s gglo -o zhmCJK.gls zhmCJK.glo
+latex zhmCJK.dtx
+latex zhmCJK.dtx
+dvipdfmx zhmCJK.dvi
+if exist zhmCJK.zip del zhmCJK.zip
+zip zhmCJK zhmCJK.dtx zhmCJK.ins zhmCJK.sty zhmCJK.pdf zhmetrics.tfm texfonts.map test-zhmCJK.tex
+del zhmCJK.dvi zhmCJK.aux zhmCJK.log zhmCJK.glo zhmCJK.gls zhmCJK.idx zhmCJK.ind zhmCJK.ilg zhmCJK.out zhmCJK.tmp  zhmCJK.*~
