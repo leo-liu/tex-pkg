191a56d6aaaad145baa548c804e849d4b272b347
Liu Haiyang <leoliu.pku@gmail.com>
2012-02-02 14:55:37 +0000

    zhmCJK 更新到 v0.3： 增加新的伪粗体功能；增加别名 \setCJKromanfont；修改 map 文件名。

--- zhmCJK/Make-zhmCJK.bat
+++ zhmCJK/Make-zhmCJK.bat
@@ -8,5 +8,5 @@ latex zhmCJK.dtx
 latex zhmCJK.dtx
 dvipdfmx zhmCJK.dvi
 if exist zhmCJK.zip del zhmCJK.zip
-zip zhmCJK zhmCJK.dtx zhmCJK.ins zhmCJK.sty zhmCJK.pdf zhmetrics.tfm texfonts.map test-zhmCJK.tex
-del zhmCJK.dvi zhmCJK.aux zhmCJK.log zhmCJK.glo zhmCJK.gls zhmCJK.idx zhmCJK.ind zhmCJK.ilg zhmCJK.out zhmCJK.tmp  zhmCJK.*~
+zip zhmCJK zhmCJK.dtx zhmCJK.ins zhmCJK.sty zhmCJK.pdf zhmetrics.tfm texfonts.map zhmCJK.map test-zhmCJK.tex
+del zhmCJK.dvi zhmCJK.aux zhmCJK.log zhmCJK.glo zhmCJK.gls zhmCJK.idx zhmCJK.ind zhmCJK.ilg zhmCJK.out zhmCJK.tmp zhmCJK.hd zhmCJK.*~
