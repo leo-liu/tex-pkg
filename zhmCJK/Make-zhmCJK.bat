ae55842cb3256b8c0cbcc9ad0831c914bc1cad93
leoliu pku <leoliu.pku@gmail.com@77a52a0d-5b5a-0024-7867-7f92eaad0685>
2012-02-04 07:04:02 +0000

    zhmCJK 增加 README。生成脚本增加 tds 包。

--- zhmCJK/Make-zhmCJK.bat
+++ zhmCJK/Make-zhmCJK.bat
@@ -1,5 +1,3 @@
-if exist zhmCJK.sty del zhmCJK.sty
-if exist zhmCJK.pdf del zhmCJK.pdf
 latex zhmCJK.ins
 latex zhmCJK.dtx
 makeindex -s gind zhmCJK.idx
@@ -7,6 +5,36 @@ makeindex -s gglo -o zhmCJK.gls zhmCJK.glo
 latex zhmCJK.dtx
 latex zhmCJK.dtx
 dvipdfmx zhmCJK.dvi
+for %%i in (zhmCJK.dvi zhmCJK.aux zhmCJK.log zhmCJK.glo zhmCJK.gls zhmCJK.idx zhmCJK.ind zhmCJK.ilg zhmCJK.out zhmCJK.tmp zhmCJK.hd zhmCJK.*~ Make-zhmCJK.bat~) do del %%i
+
+if "%1"=="zip" call :zip
+if "%1"=="tds" call :tds
+shift
+if "%1"=="zip" call :zip
+if "%1"=="tds" call :tds
+exit /b
+
+:zip
 if exist zhmCJK.zip del zhmCJK.zip
-zip zhmCJK zhmCJK.dtx zhmCJK.ins zhmCJK.sty zhmCJK.pdf zhmetrics.tfm texfonts.map zhmCJK.map test-zhmCJK.tex
-del zhmCJK.dvi zhmCJK.aux zhmCJK.log zhmCJK.glo zhmCJK.gls zhmCJK.idx zhmCJK.ind zhmCJK.ilg zhmCJK.out zhmCJK.tmp zhmCJK.hd zhmCJK.*~
+zip zhmCJK zhmCJK.dtx zhmCJK.ins README.txt zhmCJK.sty zhmCJK.pdf zhmetrics.tfm texfonts.map zhmCJK.map zhmCJK-test.tex
+exit /b
+
+:tds
+if exist zhmCJK-tds.zip del zhmCJK-tds.zip
+for %%i in (doc fonts source tex) do if exist %%i rmdir /s %%i
+mkdir source\latex\zhmCJK
+copy zhmCJK.dtx source\latex\zhmCJK\
+copy zhmCJK.ins source\latex\zhmCJK\
+mkdir tex\latex\zhmCJK
+copy zhmCJK.sty tex\latex\zhmCJK\
+mkdir doc\latex\zhmCJK
+copy zhmCJK.pdf doc\latex\zhmCJK\
+copy zhmCJK-test.tex doc\latex\zhmCJK\
+copy README.txt doc\latex\zhmCJK\
+mkdir fonts\map\fontname
+copy zhmCJK.map fonts\map\fontname\
+copy texfonts.map fonts\map\fontname\
+mkdir fonts\tfm\zhmetrics
+copy zhmetrics.tfm fonts\tfm\zhmetrics\
+zip -r zhmCJK-tds source tex doc fonts
+exit /b
