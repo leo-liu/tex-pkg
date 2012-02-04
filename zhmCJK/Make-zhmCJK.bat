6cbd64fcd66cf6fb53a8ad7a5cee963dae67ac30
leoliu pku <leoliu.pku@gmail.com@77a52a0d-5b5a-0024-7867-7f92eaad0685>
2012-02-04 07:17:39 +0000

    Make-zhmCJK/Make-zhmCJK.bat 增加 clean 功能

--- zhmCJK/Make-zhmCJK.bat
+++ zhmCJK/Make-zhmCJK.bat
@@ -1,3 +1,6 @@
+@echo off
+if "%1"=="clean" goto clean
+
 latex zhmCJK.ins
 latex zhmCJK.dtx
 makeindex -s gind zhmCJK.idx
@@ -5,23 +8,26 @@ makeindex -s gglo -o zhmCJK.gls zhmCJK.glo
 latex zhmCJK.dtx
 latex zhmCJK.dtx
 dvipdfmx zhmCJK.dvi
-for %%i in (zhmCJK.dvi zhmCJK.aux zhmCJK.log zhmCJK.glo zhmCJK.gls zhmCJK.idx zhmCJK.ind zhmCJK.ilg zhmCJK.out zhmCJK.tmp zhmCJK.hd zhmCJK.*~ Make-zhmCJK.bat~) do del %%i
+call :clean_tmp
 
 if "%1"=="zip" call :zip
 if "%1"=="tds" call :tds
 shift
 if "%1"=="zip" call :zip
 if "%1"=="tds" call :tds
-exit /b
+goto end
 
 :zip
-if exist zhmCJK.zip del zhmCJK.zip
+call :clean_zip
 zip zhmCJK zhmCJK.dtx zhmCJK.ins README.txt zhmCJK.sty zhmCJK.pdf zhmetrics.tfm texfonts.map zhmCJK.map zhmCJK-test.tex
 exit /b
 
+:clean_zip
+if exist zhmCJK.zip del zhmCJK.zip
+exit /b
+
 :tds
-if exist zhmCJK-tds.zip del zhmCJK-tds.zip
-for %%i in (doc fonts source tex) do if exist %%i rmdir /s %%i
+call :clean_tds
 mkdir source\latex\zhmCJK
 copy zhmCJK.dtx source\latex\zhmCJK\
 copy zhmCJK.ins source\latex\zhmCJK\
@@ -38,3 +44,20 @@ mkdir fonts\tfm\zhmetrics
 copy zhmetrics.tfm fonts\tfm\zhmetrics\
 zip -r zhmCJK-tds source tex doc fonts
 exit /b
+
+:clean_tds
+if exist zhmCJK-tds.zip del zhmCJK-tds.zip
+for %%i in (doc fonts source tex) do if exist %%i rmdir /s /q %%i
+exit /b
+
+:clean_tmp
+for %%i in (zhmCJK.dvi zhmCJK.aux zhmCJK.log zhmCJK.glo zhmCJK.gls zhmCJK.idx zhmCJK.ind zhmCJK.ilg zhmCJK.out zhmCJK.tmp zhmCJK.hd zhmCJK.*~ Make-zhmCJK.bat~) do if exist %%i del %%i
+exit /b
+
+:clean
+call :clean_tmp
+call :clean_zip
+call :clean_tds
+for %%i in (zhmCJK.pdf zhmCJK.sty README.txt zhmCJK-test.tex) do if exist %%i del %%i
+
+:end
